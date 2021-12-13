ddb54949d0ed14742948904ef2d8cb1295b587c3
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2021-12-13 13:34:39 +0700

    Update

--- app.py
+++ app.py
@@ -5,22 +5,11 @@ import more_itertools
 import spacy
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
-
+import time
 
 st.set_page_config(page_title="context-search")
 
-model_name = st.text_area(
-    "Enter the name of the pre-trained model from sentence transformers that we are using for summarization", value="sentence-transformers/msmarco-distilbert-dot-v5")
-st.caption("This will download a new model, so it may take awhile or even break if the model is too large")
-st.caption("See the list of pre-trained models that are available here! https://www.sbert.net/docs/pretrained_models.html")
 
-granularity = st.radio(
-    "What level of granularity do you want to summarize at?", ('sentence', 'word', 'paragraph'))
-
-query = st.text_area('Enter a query')
-corpus = st.text_area('Enter a document')
-percentage = st.number_input(
-    "Enter the percentage of the text you want highlighted", max_value=1.0, min_value=0.0, value=0.3)
 window_sizes = st.text_area(
     'Enter a list of window sizes that seperated by a space')
 window_sizes = [int(i) for i in re.split("[^0-9]", window_sizes) if i != ""]
@@ -28,8 +17,15 @@ window_sizes = [int(i) for i in re.split("[^0-9]", window_sizes) if i != ""]
 
 nlp = spacy.load('en_core_web_sm')
 
-embedder = SentenceTransformer(model_name, device='cuda')
-query_embedding = embedder.encode(query, convert_to_tensor=True)
+model_name = st.text_area(
+    "Enter the name of the pre-trained model from sentence transformers that we are using for summarization", value="sentence-transformers/msmarco-distilbert-dot-v5")
+st.caption("This will download a new model, so it may take awhile or even break if the model is too large")
+st.caption("See the list of pre-trained models that are available here! https://www.sbert.net/docs/pretrained_models.html")
+model = SentenceTransformer(model_name, device='cuda')
+
+query = st.text_area('Enter a query')
+query_embedding = model.encode([query], convert_to_tensor=True).to('cuda')
+
 granularized_corpus = []  # ["", ...]
 windowed_granularized_corpus = {}  # {window_size: [("",...), ...]}
 # {window_size: [({"corpus": "", "index": 0}, ...), ...]}
@@ -37,14 +33,21 @@ windowed_granularized_corpus_indexed = {}
 semantic_search_result = {}  # {window_size: {"corpus_id": 0, "score": 0}}
 final_semantic_search_result = {}  # {corpus_id: {"score_mean": 0, count: 0}}
 
+granularity = st.radio(
+    "What level of granularity do you want to summarize at?", ('sentence', 'word', 'paragraph'))
+corpus = st.text_area('Enter a document')
+window_sizes = st.text_area(
+    'Enter a list of window sizes that seperated by a space')
+window_sizes = [int(i) for i in re.split("[^0-9]", window_sizes) if i != ""]
+
+t0 = time.time()
+
 if granularity == "sentence":
     doc = nlp(corpus)
     granularized_corpus = [str(sent) for sent in doc.sents]
 elif granularity == "word":
     doc = nlp(corpus)
-    for sent in doc.sents:  # sentece break
-        words_per_sentence = [word for word in str(sent).split()]
-        granularized_corpus += words_per_sentence
+    granularized_corpus += corpus.split()
 elif granularity == "paragraph":
     granularized_corpus = corpus.splitlines()
 
@@ -64,13 +67,12 @@ for window_size in window_sizes:
 for window_size in window_sizes:
     corpus_len = len(windowed_granularized_corpus[window_size])
     if(window_size == 1):
-        corpus_embeddings = embedder.encode(
-            [x[0] for x in windowed_granularized_corpus[window_size]], convert_to_tensor=True)
-    else:
-        corpus_embeddings = embedder.encode(
-            windowed_granularized_corpus[window_size], convert_to_tensor=True)
+        windowed_granularized_corpus[window_size] = [x[0]
+                                                     for x in windowed_granularized_corpus[window_size]]
+    corpus_embeddings = (model.encode(
+        windowed_granularized_corpus[window_size], convert_to_tensor=True))
     semantic_search_result[window_size] = util.semantic_search(
-        query_embedding, corpus_embeddings, top_k=corpus_len)
+        query_embedding, corpus_embeddings, top_k=corpus_len, score_function=util.dot_score)
 
     for ssr in semantic_search_result[window_size][0]:
         for ssrw in windowed_granularized_corpus_indexed[window_size][ssr["corpus_id"]]:
@@ -90,6 +92,8 @@ for window_size in window_sizes:
 
 print_corpus = granularized_corpus[:]
 cleaned_raw_result = []
+percentage = st.number_input(
+    "Enter the percentage of the text you want highlighted", max_value=1.0, min_value=0.0, value=0.3)
 top_k = int(np.ceil(len(print_corpus)*percentage))
 total_score_mean = total_count = 0
 for key, val in sorted(final_semantic_search_result.items(), key=lambda x: x[1]["score_mean"], reverse=True)[:top_k]:
@@ -102,13 +106,17 @@ for key, val in sorted(final_semantic_search_result.items(), key=lambda x: x[1][
     total_score_mean = new_score_mean
 
     cleaned_raw_result.append(
-        {"corpus_id": key, "score_mean": val["score_mean"]})
+        {"corpus_id": key, "score": val["score_mean"]})
 
     annotated = "\u0332".join(print_corpus[key])
     print_corpus[key] = annotated
 
+t1 = time.time()
 
-st.subheader("Total score mean")
+st.subheader("Output process duration")
+st.write("{} ms" % (t1-t0))
+
+st.subheader("Output total score mean")
 st.write(total_score_mean)
 
 st.subheader("Output summary")
