59fb7a4b5f674d9388cf80f88c09048cbd58f779
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2021-12-18 15:24:26 +0700

    Update: && git push

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def get_granularized_corpus(corpus, granularity, window_sizes):
 granularized_corpus = get_granularized_corpus(
     corpus, granularity, window_sizes)
 
-print(granularized_corpus)
+st.write(granularized_corpus)
 
 
 @st.cache(hash_funcs={torch.Tensor: hash_tensor})
@@ -142,7 +142,7 @@ def search(model_name, scoring_technique, query, window_sizes):
 
 search_result = search(model_name, scoring_technique, query, window_sizes)
 
-print(search_result)
+st.write(search_result)
 
 percentage = st.number_input(
     "Enter the percentage of the text you want highlighted.", max_value=1.0, min_value=0.0, value=0.3)
