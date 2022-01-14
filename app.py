c7520ba995b62655987ee082e7175f1dda340f01
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2022-01-14 08:39:05 +0700

    Update

--- app.py
+++ app.py
@@ -47,10 +47,10 @@ def get_embeddings(model_name, data):
 
 corpus = st.text_area('Enter a document.')
 granularity = st.radio(
-    "What level of granularity do you want to search at?", ('word', 'sentence', 'paragraph'), index='2')
+    "What level of granularity do you want to search at?", ('word', 'sentence', 'paragraph'), index=2)
 query = st.text_area('Enter a query.')
 window_sizes = st.text_area(
-    'Enter a list of window sizes that seperated by a space.', value='1')
+    'Enter a list of window sizes that seperated by a space.', value=1)
 window_sizes = [int(i) for i in re.split("[^0-9]", window_sizes) if i != ""]
 
 
