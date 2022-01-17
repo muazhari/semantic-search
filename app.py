f46de7bbf38dc42099d5f98ac73fbc84c14ed354
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2022-01-17 17:42:51 +0700

    Update version 2

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def hash_tensor(x):
     return bio.getvalue()
 
 
-@st.cache(hash_funcs={torch.Tensor: hash_tensor, tokenizers.Tokenizer: None})
+@st.cache(hash_funcs={torch.Tensor: hash_tensor, tokenizers.Tokenizer: lambda x: None})
 def get_embeddings(model_name, data):
     embeddings = Embeddings(
         {"path": model_name, "content": True, "objects": True})
@@ -142,7 +142,7 @@ def rerank_search(queries, embeddings, similarity, limit):
     return [{"corpus_id": id, "score": score} for id, score in similarity(queries, results)]
 
 
-@st.cache(hash_funcs={torch.Tensor: hash_tensor, tokenizers.Tokenizer: None})
+@st.cache(hash_funcs={torch.Tensor: hash_tensor})
 def search(model_name, query, window_sizes, granularized_corpus):
     semantic_search_result = {}  # {window_size: {"corpus_id": 0, "score": 0}}
     final_semantic_search_result = {}  # {corpus_id: {"score_mean": 0, count: 0}}
