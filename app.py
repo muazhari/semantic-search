a0dc32fa311099412441354b07650acd7500ae4b
Muhammad Kharisma Azhari <39398937+muazhari@users.noreply.github.com>
2022-02-01 17:06:43 +0700

    Update v2

--- app.py
+++ app.py
@@ -380,8 +380,8 @@ def get_annotated_pdf(search_result_raw, granularized_corpus_raw, input_path, ou
 
 
 html_pdf = None
-if (None not in [corpus, filtered_search_result, shaped_corpus]):
-    if (corpus_source_type in ["document", "web"]):
+if (None not in [corpus, filtered_search_result, shaped_corpus, corpus_source_type, output_type]):
+    if (corpus_source_type in ["document", "web"] and output_type in ["default"]):
         file_name = os.path.splitext(os.path.basename(corpus))[0]
         highlighted_file_base_name = f'{file_name}_highlighted_{percentage}.pdf'
         highlighted_file_path = str(ASSETS_PATH / highlighted_file_base_name)
